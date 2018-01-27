9f82a800aa29f80fb34bd2e8c5739f34b0d92b76
Joshua Lease <klezst@gmail.com>
2018-01-27 16:54:00 -0500

    Run simulation on all emitters

--- Assets/Scripts/Emit.cs
+++ Assets/Scripts/Emit.cs
@@ -6,15 +6,7 @@ public class Emit : MonoBehaviour {
     public GameObject emitted;
     public Transform spawn;
 
-	void Start () {
-        //Instantiate(emitted, spawn.position, spawn.rotation);
-	}
-
     public void Transmit() {
         Instantiate(emitted, spawn.position, spawn.rotation);
-    }
-
-    private void OnMouseDown() {
-        Debug.Log("RAWR");
     }
 }
