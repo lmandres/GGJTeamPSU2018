ae2d786641eb853bfaafaf4a39e08aff2be60ef8
Joshua Lease <klezst@gmail.com>
2018-01-26 22:26:24 -0500

    Update radio wave to use animated sprite

--- Assets/Scripts/Project.cs
+++ Assets/Scripts/Project.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class Project : MonoBehaviour {
     public float speed;
+    public float strength;
 
 	void Start () {
         GetComponent<Rigidbody2D>().AddRelativeForce(new Vector2(1.0f * speed, 0));
+    }
+
+    private void FixedUpdate() {
+        strength--;
+        if (strength < 1) {
+            Destroy(gameObject);
+        }
     }
 }
