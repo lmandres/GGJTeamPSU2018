20cffe349a28c2f1a689ad69d45ab04a5ec23c65
profcommie <profcommie@gmail.com>
2018-01-28 12:35:09 -0500

    Adding MainCamera w/ Music and win logic

---
+++ Assets/Scripts/Win.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Win : MonoBehaviour {
+
+	public int nextLevel;
+
+	private void OnTriggerEnter2D(Collider2D collision) {
+        GUI.contentColor = Color.red;
+		if(GUI.Button(new Rect((Screen.width/3) * 2,Screen.height/2 +100,100,40),"Next Level")){
+			Application.LoadLevel(nextLevel);
+		}
+		//and quit button
+		if(GUI.Button(new Rect((Screen.width/3) * 2,Screen.height/2 +150,100,40),"Quit")){
+			Application.Quit();
+    }
+}
