3c7e7ce68d0303468b2419eba6c2986c35b1d772
profcommie <profcommie@gmail.com>
2018-01-27 21:05:15 -0500

    drops angle check from split

--- Assets/Scripts/Split.cs
+++ Assets/Scripts/Split.cs
@@ -14,13 +14,14 @@ public class Split : MonoBehaviour {
     public string tag;
 
     private void OnTriggerEnter2D(Collider2D collision) {
+        Debug.Log("Receiver is Hit");
         if (collision.gameObject.tag != tag) {
             return;
         }
         // Ignore if the object faces away from the recieving satellite
-        if (Quaternion.Angle(gameObject.transform.rotation, collision.transform.rotation) >= 90) {
+        /*if (Quaternion.Angle(gameObject.transform.rotation, collision.transform.rotation) >= 270) {
             return;
-        }
+        }*/
         Destroy(collision.gameObject);
         foreach (Transform spawn in spawns) {
             Instantiate(emitted, spawn.position, spawn.rotation);
