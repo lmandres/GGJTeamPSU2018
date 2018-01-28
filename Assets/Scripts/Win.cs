5758d7f2b8ff1fc87ca41de9f683cb3b331c3c6a
profcommie <profcommie@gmail.com>
2018-01-28 14:52:43 -0500

    Win.cs logic

--- Assets/Scripts/Win.cs
+++ Assets/Scripts/Win.cs
@@ -3,8 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Win : MonoBehaviour {
+	GameObject WinCanvas;
+
+	void Start () {
+		WinCanvas = GameObject.Find("Win Canvas");
+		WinCanvas.SetActive(false);
+	}
 
 	private void OnTriggerEnter2D(Collider2D collision) {
-        
+        WinCanvas.SetActive(true);
 	}
 }
