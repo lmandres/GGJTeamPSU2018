717a3895d8dfff69835d0695b442198950838fc2
profcommie <profcommie@gmail.com>
2018-01-28 13:47:41 -0500

    Logic for WinCanvas

---
+++ Assets/Scripts/winButtonClick.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class winButtonClick : MonoBehaviour {
+
+	public bool isLastLevel;
+	public int nextLvl;
+	GameObject nextLevelBtn;
+	GameObject mainMenuBtn;
+
+	void Start () {
+		nextLevelBtn = GameObject.Find("NextLevel");
+		mainMenuBtn = GameObject.Find("MainMenu");
+		if(isLastLevel){
+			nextLevelBtn.SetActive(false);
+		}
+	}
+
+	public void NextLevel(){
+		Application.LoadLevel(nextLvl);
+	}
+
+	public void MainMenu(){
+		Application.LoadLevel(0);
+	}
+}
