ddf6e304b9af61356a365295fc3643cade501f11
Leo Andres <lmandres@yahoo.com>
2018-01-31 21:13:42 -0500

    Reset the settings for advancing the game
    Reset the settings for advancing the game.  To advance the
    game, the Button (Script) is either checked or unchecked.
    To enable the button within the game, get the button object,
    and set it to enabled.  (See Win.cs script)

--- Assets/Scripts/winButtonClick.cs
+++ Assets/Scripts/winButtonClick.cs
@@ -7,31 +7,10 @@ public class winButtonClick : MonoBehaviour {
 
 	public bool isLastLevel;
 	public int nextLvl;
-	GameObject nextLevelBtn;
-	GameObject mainMenuBtn;
-
-    void Start () {
-		nextLevelBtn = GameObject.Find("NextLevel");
-		mainMenuBtn = GameObject.Find("MainMenu");
-		if(isLastLevel){
-			nextLevelBtn.SetActive(false);
-		}
-	}
 
 	public void NextLevel()
     {
-        Component child = nextLevelBtn.GetComponentInChildren(typeof(Button));
-        Button childButton = (Button)child;
-
-        if (nextLvl%2 > 0)
-        {
-            childButton.enabled = true;
-        } else
-        {
-            childButton.enabled = false;
-        }
         Application.LoadLevel(nextLvl);
-
     }
 
 	public void MainMenu(){
